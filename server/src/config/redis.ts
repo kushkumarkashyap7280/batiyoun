4b102ddd7011b42639a223659c80eaf4be3c2d72
kush kumar <kushkumarking098765@gmail.com>
2026-02-03 15:30:01 +0530

    feat: integrate Upstash Redis for improved caching and data management

---
+++ server/src/config/redis.ts
@@ -0,0 +1,7 @@
+import Redis from "ioredis";
+
+
+export const redis = new Redis(process.env.REDIS_URI || "");
+
+redis.on("connect", () => console.log("✅ Redis (TCP) Connected"));
+redis.on("error", (err) => console.error("❌ Redis Error:", err));
